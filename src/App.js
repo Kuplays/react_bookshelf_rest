19541b5170f28a4a1d79bf0d1e3952c0775163ff
kuplays <alexkuplays@gmail.com>
2020-10-27 00:02:35 +0700

    added cors and basic rest point fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
+  const [appState, setAppState] = useState({
+    loading: false,
+    books: null,
+  });
+
+  useEffect(() => {
+    setAppState({loading: true});
+    const REST_URL_GET_ALL = 'http://localhost:3001/all';
+    axios.get(REST_URL_GET_ALL).then((books) => {
+      const allBooks = books.data;
+      setAppState({loading:false, books: allBooks});
+    });
+  }, [setAppState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>
+          All books test fetch on rest point
+        </h1>
+      </div>
+      <div>
+        <BookList isLoading={appState.loading} books={appState.books} />
+      </div>
     </div>
   );
 }
