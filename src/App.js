0b26576e91465a96798110a5c92e921b9a7bda76
kuplays <alexkuplays@gmail.com>
2020-10-27 23:47:47 +0700

    rework App main and added AddBookForm

--- src/App.js
+++ src/App.js
@@ -2,34 +2,59 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import BookList from './components/BookList';
 import axios from 'axios';
+import {AddBookForm} from './components/AddBookForm';
 
-function App() {
-  const [appState, setAppState] = useState({
-    loading: false,
-    books: null,
-  });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      books: null
+    };
+  }
 
-  useEffect(() => {
-    setAppState({loading: true});
-    const REST_URL_GET_ALL = 'http://localhost:3001/all';
-    axios.get(REST_URL_GET_ALL).then((books) => {
-      const allBooks = books.data;
-      setAppState({loading:false, books: allBooks});
-    });
-  }, [setAppState]);
 
-  return (
-    <div className='App'>
-      <div className='container'>
-        <h1>
-          All books test fetch on rest point
-        </h1>
-      </div>
-      <div>
-        <BookList isLoading={appState.loading} books={appState.books} />
-      </div>
-    </div>
-  );
+  render() {
+    const {isLoading, books} = this.state;
+    return (
+      <p>stub</p>   
+    )
+  }
+
+  
 }
 
 export default App;
+
+
+// function App() {
+//   const [appState, setAppState] = useState({
+//     loading: false,
+//     books: null,
+//   });
+
+//   useEffect(() => {
+//     setAppState({loading: true});
+//     const REST_URL_GET_ALL = 'http://localhost:3001/all';
+//     axios.get(REST_URL_GET_ALL).then((books) => {
+//       const allBooks = books.data;
+//       setAppState({loading:false, books: allBooks});
+//     });
+//   }, [setAppState]);
+
+//   return (
+//     <div className='App'>
+//       <AddBookForm />
+//       <div className='container'>
+//         <h1>
+//           All books test fetch on rest point
+//         </h1>
+//       </div>
+//       <div>
+//         <BookList isLoading={appState.loading} books={appState.books} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/AddBookForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class AddBookForm extends React.Component {
+	render() {
+		return (
+			<p>test</p>
+		);
+	}
+}
