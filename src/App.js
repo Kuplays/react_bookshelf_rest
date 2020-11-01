7cfd018aeb68636f1e2416a4d25ed1f8f212d3f3
kuplays <alexkuplays@gmail.com>
2020-11-02 00:53:36 +0700

    integrated bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import BookList from './components/BookList';
 import AxiosParams from './utils/AxiosParams';
 import AddBookForm from './components/AddBookForm';
+import navbarLogo from './logo.svg'
 
 class App extends React.Component {
   constructor(props) {
@@ -22,6 +23,23 @@ class App extends React.Component {
   render() {
     return (
         <div className='App'>
+        <nav className='navbar sticky-top navbar-light bg-light'>
+          <a className='navbar-brand' href='http://localhost:3000'>
+            <img src={navbarLogo} width='50' height='50' alt='react logo'/>
+            REACTive LIBRARY
+          </a>
+          <ul className='list-inline'>
+            <li className='list-inline-item'>
+              <a className='nav-link' href='#'>Home</a>
+            </li>
+            <li className='list-inline-item'>
+              <a className='nav-link' href='#'>Add Book</a>
+            </li>
+            <li className='list-inline-item'>
+              <a className='nav-link' href='#'>User Statistics</a>
+            </li>
+          </ul>
+        </nav>
          <AddBookForm toggleState={this.toggleLoadingState}/>
             <h1>
               REACTive BOOK LIBRARY
